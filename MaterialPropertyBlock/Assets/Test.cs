bef35f4b638a1342619896dcc733b48c2ea608f2
ysfmertyldzz <77334914+ysfmertyldzz@users.noreply.github.com>
2021-07-03 14:08:55 +0300

    Material Property Block

---
+++ MaterialPropertyBlock/Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public Color color = Color.black;
+
+    Renderer renderer;
+    MaterialPropertyBlock propertyBlock;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        propertyBlock.SetColor("_Color", color);
+        renderer.SetPropertyBlock(propertyBlock);
+    }
+}
